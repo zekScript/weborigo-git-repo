60271a8fd482fce5fd6e7ae25f6ce1562905d35a
MadMatas <164335636+MadMatas@users.noreply.github.com>
2025-11-13 15:46:33 +0100

    QR and Passwd gens done (for now)

--- qrcode_menu/content.jsx
+++ qrcode_menu/content.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import QRCode from "qrcode";
 
+
 const QRGenerator = () => {
   const [text, setText] = useState("");
   const [size, setSize] = useState(200);
