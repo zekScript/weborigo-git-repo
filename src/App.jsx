14895a66fb32b1b802c19eb71a180c5f3974f28e
zekScript <armandasdarbinis@gmail.com>
2025-11-14 10:46:47 +0100

    Moved some things and made style

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import { createBrowserRouter } from 'react-router'
 import PasswordGenerator from './pages/PasswordGenerator'
-import QRFrame from '../qrcode_menu/iframe'
-import QRGenerator from '../qrcode_menu/content'
+import QRGenerator from './pages/qrcode_menu/content'
+import Home from './pages/Home/Home'
 
 function App() {
 
   const route = createBrowserRouter([
     {
-      element: <h1>Home Page</h1>,
+      element: <Home/>,
       path: "/"
     },
     {
